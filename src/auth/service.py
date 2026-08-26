1da9bc58150a39159cae9316a15b7ab1dc6a3f1a
Smudge <JXS1787@student.bham.ac.uk>
2026-08-26 22:06:10 +0100

    Finished authentication

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,11 +1,16 @@
 import os
 import logging
+import jwt
 from dotenv import load_dotenv
 from uuid import UUID, uuid4
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from typing import Annotated
 from src.entities.user import User
+from src.exceptions import AuthenticationError
 from . import schemas
 
 
@@ -36,3 +41,38 @@ def register_user(database: Session, register_user_request: schemas.RegisterUser
     except Exception as e:
         logging.error(f"Failed to register user: {register_user_request.email}. Error: {str(e)}")
         raise
+
+
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                           database: Session) -> schemas.Token:
+    user = authenticate_user(form_data.username, form_data.password, database)
+    if not user: raise AuthenticationError()
+
+    token = create_access_token(user.email, user.id, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return schemas.Token(access_token=token, token_type='bearer')
+
+
+""" Helper Functions """
+def get_password_hash(password: str) -> str:
+    return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(email: str, password: str, db: Session) -> User | bool:
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not verify_password(password, user.password_hash):
+        logging.warning(f"Failed authentication attempt for email: {email}")
+        return False
+    return user
+
+
+def create_access_token(email: str, user_id: UUID, expires_delta: timedelta) -> str:
+    encode = {
+        'sub': email,
+        'id': str(user_id),
+        'exp': datetime.now(timezone.utc) + expires_delta
+    }
+    return jwt.encode(encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
